286934076e525c972cf1a8c39af261cad15ba913
Milan Stevanovic <milan.wittg@gmail.com>
2022-10-26 03:25:14 +0200

    initial commit

---
+++ src/main/java/com/example/itbcloggerfinalproject/ItbcLoggerFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.itbcloggerfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbcLoggerFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbcLoggerFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/itbcloggerfinalproject/ItbcLoggerFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itbcloggerfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbcLoggerFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
